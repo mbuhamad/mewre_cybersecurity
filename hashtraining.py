f161ff10fc5aef974e54e96d9c89cb5e9c473d1f
mbuhamad <mbuhamad@gmail.com>
2022-04-09 21:41:27 +0300

    another update

---
+++ hashtraining.py
@@ -0,0 +1,14 @@
+from sys import argv as inp
+inp.pop(0)
+inp=(' '.join(inp))
+print (inp)
+import hashlib
+m=hashlib.sha256()
+text=inp.encode ()
+m.update(text)
+dig=m.hexdigest()
+print(dig)
+n=hashlib.md5()
+n.update(text)
+dig2=n.hexdigest()
+print(dig2)
