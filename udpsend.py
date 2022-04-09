4782095436e69f93abaf12c385c28cc3a1e65bd5
mbuhamad <mbuhamad@gmail.com>
2022-04-10 02:15:43 +0300

    update

--- udpsend.py
+++ udpsend.py
@@ -7,10 +7,10 @@ import socket
 UDP_IP = "127.0.0.1" # set the up address of the local machine
 UDP_PORT = 965
 while True:
-	MESSAGE = input("Enter your msg here:")
+	MESSAGE = input('Enter your msg here:')
 	MESSAGE = MESSAGE.encode('utf-8') 
-	print("UDP target IP: %s" % UDP_IP)
-	print("UDP target port: %s" % UDP_PORT)
-	print("message: %s" % MESSAGE.decode()) 
+	print('UDP target IP:',UDP_IP)
+	print('UDP target port:',UDP_PORT)
+	print('message sent:',MESSAGE.decode()) 
 	sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 	sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
